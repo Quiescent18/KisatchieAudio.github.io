7868a528f1f8d5867e93b701a438e101168b2646
Quiescent18 <quiescent18@yahoo.com>
2025-07-19 08:56:07 -0500

    Update tracks.js

--- tracks.js
+++ tracks.js
@@ -3,80 +3,80 @@ const tracks = [
     title: "Bright Fires - Paragraphs & Parallels",
     file: "audio/BRIGHT FIRES - PARAGRAPHS & PARALLELS FINAL (MP3).mp3",
     artwork: "img/portfolio/Para cover.jpeg",
-    credits: ["Mixing", "Mastering", "Production"],
+    credits: ["Mixing", "Mastering", "Production", "Recording"],
     genres: ["Rock", "Emo"]
   },
   {
     title: "Bright Fires - A Way With Words",
     file: "audio/bright fires - a way with words M10.mp3",
     artwork: "img/portfolio/a way.jpg",
-    credits: ["Mixing", "Mastering", "Production"],
+    credits: ["Mixing", "Mastering", "Production", "Recording"],
     genres: ["Rock", "Emo"]
     //disclaimer: "Not the official mix.\nCourtesy of XYZ Academy."
   },
   {
-    title: "Last II - Black Hole",
-    file: "audio/BlackHole.mp4",
-    artwork: "img/portfolio/last2.webp",
-    credits: ["Keyboards"],
-    genres: ["Rock", "Metal"]
+    title: "Bright Fires - Shadows",
+    file: "audio/bright fires - shadows final 7.mp3",
+    artwork: "img/portfolio/bright fires - shadows cover.jpeg",
+    credits: ["Mixing", "Mastering", "Production", "Recording"],
+    genres: ["Rock", "Emo"]
   },
   {
-    title: "FXRLY - I'll Be There",
-    file: "audio/bthere.mp3",
-    artwork: "img/portfolio/FXLRY.webp",
-    credits: ["Keyboards"],
+    title: "If I'm Lucky - Deja Vu",
+    file: "audio/if im lucky - deja vu.mp3",
+    artwork: "img/portfolio/dejavu.png",
+    credits: ["Mixing", "Mastering"],
     genres: ["Pop Punk"]
   },
   {
-    title: "Tyler Montague March - Pink Pony Club",
-    file: "audio/pinkpony.mp3",
-    artwork: "img/portfolio/pinkpony.webp",
-    credits: ["Mastering", "Mixing", "Keyboards", "Engineering", "Produced"],
-    genres: ["Contemporary Country"]
-  },
-  {
-    title: "McVittie - Adrenaline",
-    file: "audio/Adrenaline.mp3",
-    artwork: "img/portfolio/adrenaline.webp",
-    credits: ["Keyboards", "Produced", "Mixing", "Mastering", "Writing", "Engineering"],
-    genres: ["Electronic", "Synthwave"]
-  },
-  {
-    title: "Spectra - Meridian",
-    file: "audio/Meridian.mp3",
-    artwork: "img/portfolio/pwodo.webp",
-    credits: ["Keyboards", "Writing", "Produced", "Mastering", "Mixing", "Engineering"],
-    genres: ["Progressive Rock"]
-  },
-  {
-    title: "McVittie - Lone Killer",
-    file: "audio/LoneKiller.mp3",
-    artwork: "img/portfolio/lonekiller.webp",
-    credits: ["Keyboards", "Produced", "Mixing", "Mastering", "Writing", "Engineering"],
-    genres: ["Electronic", "Synthwave"]
-  },
-  {
-    title: "Crimson Ash - In Twilight",
-    file: "audio/InTwilight.mp3",
-    artwork: "img/portfolio/meltingsky.webp",
+    title: "Cardinals Pride - What Moves Us",
+    file: "audio/cardinals pride - what moves us.mp3",
+    artwork: "img/portfolio/cardinals.png",
     credits: ["Mixing", "Mastering"],
-    genres: ["Progressive Rock"]
+    genres: ["Metal"]
   },
   {
-    title: "Last II - Never Forget",
-    file: "audio/neverforget.mp4",
-    artwork: "img/portfolio/last2.webp",
-    credits: ["Keyboards"],
-    genres: ["Rock", "Metal"]
+    title: "Ash Gale - We Got the Fire",
+    file: "audio/ASH GALE- WE GOT THE FIRE.mp3",
+    artwork: "img/portfolio/ash g.png",
+    credits: ["Mixing", "Mastering"],
+    genres: ["Pop Rock"]
+  },
+  {
+    title: "Voila ft The Ready Set - We Could Be The Future",
+    file: "audio/voila ft the ready set - we could be the future.mp3",
+    artwork: "img/portfolio/voila.png",
+    credits: ["Mixing", "Mastering"],
+    genres: ["Rock"]
+  },
+  {
+    title: "Trees On Mars - First Place",
+    file: "audio/trees on mars- first place.mp3",
+    artwork: "img/portfolio/trees.png",
+    credits: ["Mixing", "Mastering"],
+    genres: ["Rock, Instrumental"]
+  },
+  {
+    title: "Drew Elliott - Conversations",
+    file: "audio/drew elliott - conversations.mp3",
+    artwork: "img/portfolio/drew.png",
+    credits: ["Mixing", "Mastering"],
+    genres: ["Alternative", "Indie"]
+  },
+  {
+    title: "Signifier - Single",
+    file: "audio/SIGNIFIER - SINGLE.mp3",
+    artwork: "img/portfolio/signifier.png",
+    credits: ["Mixing", "Mastering"],
+    genres: ["Metal"]
   },
   
   {
-    title: "Reggo The God - Birds Eye View",
-    file: "audio/birdseyeview.mp3",
+    title: "Burst and Bloom - Liar",
+    file: "audio/burst and bloom - liar.mp3",
     artwork: "img/portfolio/birdseye.webp",
-    credits: ["Mastering", "Mixing"],
-    genres: ["Hip Hop"]
+    credits: ["Mixing", "Mastering"],
+    genres: ["Rock"]
   },
   {
     title: "Spectra - Lost At Sea",
